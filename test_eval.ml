bd682d27512c5d90a9645f4b0020d8233741e859
Nikita <ng354@cornell.edu>
2015-10-28 13:43:18 -0400

    test cases are done for eval

--- test_eval.ml
+++ test_eval.ml
@@ -4,8 +4,6 @@ open Assertions
 
 TEST_UNIT = eval [] (Int 42) === VInt 42
 
-(* TODO: write more unit tests for [eval] *)
-
 (* Tests Unit, Bool, String *)
 TEST_UNIT = eval [] (Unit) === VUnit
 TEST_UNIT = eval [] (Bool true) === VBool true
@@ -18,6 +16,9 @@ TEST_UNIT = eval [] (BinOp(Plus, Int 2, BinOp(Plus, Int 3, Int 4))) === VInt 9
 TEST_UNIT = eval [] (BinOp(Plus, BinOp(Plus, Int 3, Int 4), Int 2)) === VInt 9
 TEST_UNIT = eval [] (BinOp(Plus, BinOp(Plus, Int 3, Int 4), BinOp(Plus, Int 3, Int 4))) === VInt 14
 TEST_UNIT = eval [] (BinOp (Plus, Int 4, Bool true)) === VError "Not a valid value"
+TEST_UNIT = eval [] (BinOp (Plus, String "hey", Bool true)) === VError "Not a valid value"
+TEST_UNIT = eval [] (BinOp (Plus, Bool false, Bool true)) === VError "Not a valid value"
+TEST_UNIT = eval [] (BinOp (Plus, Int 4, Unit)) === VError "Not a valid value"
 
 (* Test BinOp Minus *)
 TEST_UNIT = eval [] (BinOp(Minus, Int 10, Int 2)) === VInt 8
@@ -29,6 +30,7 @@ TEST_UNIT = eval [] (BinOp(Minus, String "hey", Int 4)) === VError "Not a valid 
 (* Test combination of Plus and Minus *)
 TEST_UNIT = eval [] (BinOp (Minus, BinOp (Plus, Int 3, Int 5), BinOp (Plus, Int 4, Int 1))) === VInt 3
 TEST_UNIT = eval [] (BinOp (Minus, BinOp (Plus, Int 3, Int 5), Int 1)) === VInt 7
+TEST_UNIT = eval [] (BinOp (Minus, BinOp (Plus, Bool false, Int 5), Int 1)) === VError "Not a valid value"
 
 (* Test BinOp Times *)
 TEST_UNIT = eval [] (BinOp(Times, Int 10, Int 2)) === VInt 20
@@ -42,23 +44,31 @@ TEST_UNIT = eval [] (BinOp (Lt, Int 4, Int 6)) === VBool true
 TEST_UNIT = eval [] (BinOp (Lt, Int 10, Int 6)) === VBool false
 TEST_UNIT = eval [] (BinOp (Lt, Bool true, Bool true)) === VBool false
 TEST_UNIT = eval [] (BinOp (Lt, Unit, Unit)) === VBool false
+TEST_UNIT = eval [] (BinOp (Lt, String "hello", String "hello")) === VBool false
 
 TEST_UNIT = eval [] (BinOp (Gt, Int 10, Int 6)) === VBool true
 TEST_UNIT = eval [] (BinOp (Gt, Int 4, Int 6)) === VBool false
 TEST_UNIT = eval [] (BinOp (Gt, Bool true, Bool true)) === VBool false
 TEST_UNIT = eval [] (BinOp (Gt, Unit, Unit)) === VBool false
+TEST_UNIT = eval [] (BinOp (Gt, String "goodbye", String "hello")) === VBool false
 
 TEST_UNIT = eval [] (BinOp (Eq, Int 4, Int 6)) === VBool false
 TEST_UNIT = eval [] (BinOp (Eq, Int 6, Int 6)) === VBool true
+TEST_UNIT = eval [] (BinOp (Ast.Eq, String "goodbye", String "hello")) === VBool false
 
 TEST_UNIT = eval [] (BinOp (GtEq, BinOp(Plus, Int 10, Int 2), Int 6)) === VBool true
 TEST_UNIT = eval [] (BinOp (GtEq, Int 6, Int 6)) === VBool true
+TEST_UNIT = eval [] (BinOp (GtEq, Bool true, Bool false)) === VBool true
 
 TEST_UNIT = eval [] (BinOp (LtEq, Int 10, Int 6)) === VBool false
 TEST_UNIT = eval [] (BinOp (LtEq, BinOp(Minus, Int 6,Int 4), Int 6)) === VBool true
+TEST_UNIT = eval [] (BinOp (LtEq, String "hello", String "hello")) === VBool true
+TEST_UNIT = eval [] (BinOp (LtEq, Bool true, Bool false)) === VBool false
 
 TEST_UNIT = eval [] (BinOp (NotEq, Int 4, Int 6)) === VBool true
 TEST_UNIT = eval [] (BinOp (NotEq, Int 6, Int 6)) === VBool false
+TEST_UNIT = eval [] (BinOp (NotEq, Bool true, Bool false)) === VBool true
+TEST_UNIT = eval [] (BinOp (NotEq, Unit, Bool false)) === VError "Not a valid value"
 
 TEST_UNIT = eval [] (BinOp (GtEq, BinOp (Times, Int 10, BinOp (Plus, Int 3, Int 5)),
                           BinOp (Minus, Int 14, BinOp (Times, Int 7, Int 1))))=== VBool true
@@ -66,6 +76,7 @@ TEST_UNIT = eval [] (BinOp (GtEq, BinOp (Times, Int 10, BinOp (Plus, Int 3, Int 
 (* Test Concat *)
 TEST_UNIT = eval [] (BinOp (Concat, String("hello "), String("goodbye"))) === VString "hello goodbye"
 TEST_UNIT = eval [] ( BinOp (Concat, Bool true, String "goodbye")) === VError "Not a valid value"
+TEST_UNIT = eval [] (BinOp (Concat, Int 4, Int 5)) === VError "Not a valid value"
 
 (* End of BinOp tests *)
 
@@ -74,13 +85,18 @@ TEST_UNIT = eval [] (If(Bool true, Int 4, Int 6)) === VInt 4
 TEST_UNIT = eval [] (If(Bool false, Int 4, Int 6)) === VInt 6
 TEST_UNIT = eval [] (If(Bool false, Int 4, String "false!!")) === VString "false!!"
 TEST_UNIT = eval [] (If(BinOp (Gt, Int 3, Int 2), Int 4, Int 6)) === VInt 4
+TEST_UNIT = eval [] (If (BinOp (NotEq, BinOp (Times, Int 4, Int 7), Int 28), String "wrong", String "right")) === VString "right"
 
 (* Test Var *)
 let var_env = ("x", ref (VInt 5))::[]
 TEST_UNIT = eval var_env (Var "x") === VInt 5
+TEST_UNIT = eval [] (Let ("x", BinOp (Plus, Int 5, Int 6), BinOp (Times, Var "x", Int 8))) === VInt 88
 
 (* Test App, assuming Fun is correct (see below) *)
 TEST_UNIT = eval [] (App (Fun ("x", BinOp (Plus, Var "x", Int 1)), BinOp (Plus, Int 5, Int 7))) === VInt 13
+TEST_UNIT = eval [] (Fun ("x", App (BinOp (LtEq, Var "x", Int 45), BinOp (Times, Int 9, Int 4)))) ===
+                    (VClosure ("x", App (BinOp (LtEq, Var "x", Int 45), BinOp (Times, Int 9, Int 4)), []) )
+TEST_UNIT = eval [] (App (Fun ("x", BinOp (LtEq, Var "x", Int 45)), BinOp (Times, Int 9, Int 4))) === VBool true
 
 TEST_UNIT = eval [] (
  App (Fun ("x", BinOp (Plus, Var "x", Int 1)),
@@ -110,17 +126,19 @@ TEST_UNIT = eval [] (Pair (App (Fun ("x", BinOp (Plus, Var "x", Int 1)), BinOp (
   BinOp (LtEq, Int 3, Int 6))   ) === VPair(VInt 11, VBool true)
 
 (* Test Variant *)
-TEST_UNIT = eval []  (Variant ("VInt", Int 3)) === VVariant("VInt", VInt 3)
-TEST_UNIT = eval [] ( Variant ("VBool", BinOp (Lt, Int 3, Int 6)) ) === VVariant("VBool", VBool true)
+TEST_UNIT = eval [] (Variant ("VInt", Int 3)) === VVariant("VInt", VInt 3)
+TEST_UNIT = eval [] (Variant ("VBool", BinOp (Lt, Int 3, Int 6)) ) === VVariant("VBool", VBool true)
 
 (* Test Let *)
 TEST_UNIT = eval [] (Let ("x", Int 5, BinOp (Plus, Var "x", Int 4))) === VInt 9
 TEST_UNIT = eval [] (Let ("x", Bool true, If (Var "x", Int 3, Int 5))) === VInt 3
-TEST_UNIT = eval [] (Let ("x", BinOp (Times, BinOp (Minus, Int 10, Int 5), Int 4),                            Let ("y", BinOp (Plus, Var "x", Int 50),
-    If (BinOp (Lt, Var "y", Int 100), Bool true, Bool false)))) === VBool true
+TEST_UNIT = eval [] (Let ("x", BinOp (Times, BinOp (Minus, Int 10, Int 5), Int 4),
+                    Let ("y", BinOp (Plus, Var "x", Int 50),
+                    If (BinOp (Lt, Var "y", Int 100), Bool true, Bool false)))) === VBool true
 (* Tests principle of name irrelevance *)
-TEST_UNIT = eval [] (Let ("x", BinOp (Times, BinOp (Minus, Int 10, Int 5), Int 4),                            Let ("x", BinOp (Plus, Var "x", Int 50),
-    If (BinOp (Ast.Eq, Var "x", Int 70), Bool true, Bool false)))) === VBool true
+TEST_UNIT = eval [] (Let ("x", BinOp (Times, BinOp (Minus, Int 10, Int 5), Int 4),
+                    Let ("x", BinOp (Plus, Var "x", Int 50),
+                    If (BinOp (Ast.Eq, Var "x", Int 70), Bool true, Bool false)))) === VBool true
 
 
 (* Test LetRec on factorial function *)
@@ -139,7 +157,6 @@ TEST_UNIT = eval [] (LetRec ("fact", Fun ("n",
     If (BinOp (Ast.Eq, Var "n", Int 1), Int 1,
      BinOp (Times, Var "n", App (Var "fact", BinOp (Minus, Var "n", Int 1))))),
    App (Var "fact", Bool false))) === VError "Not a boolean value"
-
 
 
 (* Test Match *)
